4f33a1420f6a6bc52b0d47c5cc1107c71d8b7a20
weilingxie1 <sqlnever@gmail.com>
2021-07-17 15:50:12 +1200

    Formatted Program.cs.

--- src/NewTodo/Program.cs
+++ src/NewTodo/Program.cs
@@ -12,6 +12,9 @@ namespace NewTodo
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
