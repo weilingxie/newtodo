b6e191e5dd31baba47a460bc93de29da9313e72b
Weiling Xie <xietanzhen1@yeah.net>
2021-07-22 15:55:48 +1200

    Moved Validators test to NewTodoInputValidatorTests.cs file

---
+++ NewTodo.Test/Application/TodoItem/Validators/NewTodoInputValidatorTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentValidation.TestHelper;
+using NewTodo.Application.TodoItem.Models;
+using NewTodo.Application.TodoItem.Validators;
+using Xunit;
+
+namespace NewTodo.Test.Application.TodoItem.Validators
+{
+    [Trait("Category", "UnitTest")]
+    public class NewTodoInputValidatorTests
+    {
+        private readonly NewTodoInputValidator _newTodoInputValidator;
+        private const string ValidTitle = "title";
+
+        public NewTodoInputValidatorTests()
+        {
+            _newTodoInputValidator = new NewTodoInputValidator();
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidNewTodoInput))]
+        public async Task ShouldReturnInvalid_IfProvideInvalidNewTodoInput(NewTodoInput invalidInput)
+        {
+            var result = await _newTodoInputValidator.TestValidateAsync(invalidInput);
+            Assert.False(result.IsValid);
+        }
+
+        public static IEnumerable<object[]> InvalidNewTodoInput =>
+            new List<object[]>
+            {
+                new object[] {new NewTodoInput() {UserId = Guid.Empty, Title = ValidTitle}},
+                new object[] {new NewTodoInput() {UserId = Guid.Empty, Title = string.Empty}},
+                new object[] {new NewTodoInput() {UserId = Guid.Empty, Title = null}},
+                new object[] {new NewTodoInput() {UserId = Guid.NewGuid(), Title = string.Empty}},
+                new object[] {new NewTodoInput() {UserId = Guid.NewGuid(), Title = null}}
+            };
+    }
+}
--- NewTodo.Test/Controllers/TodoControllerTests.cs
+++ NewTodo.Test/Controllers/TodoControllerTests.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using FluentValidation.TestHelper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NewTodo.Application.TodoItem.Commands;
 using NewTodo.Application.TodoItem.Models;
-using NewTodo.Application.TodoItem.Validators;
 using NewTodo.Controllers;
 using Xunit;
 using static NewTodo.Test.Helpers.TodoItemGenerator;
@@ -21,12 +17,9 @@ namespace NewTodo.Test.Controllers
         private readonly Mock<IMediator> _mediatorMock;
         private readonly TodoController _controller;
         private readonly NewTodoInput _validInput;
-        private readonly NewTodoInputValidator _newTodoInputValidator;
-        private const string ValidTitle = "title";
 
         public TodoControllerTests()
         {
-            _newTodoInputValidator = new NewTodoInputValidator();
             _mediatorMock = new Mock<IMediator>();
             _validInput = CreateValidNewTodoInputFaker().Generate();
             _controller = new TodoController(_mediatorMock.Object);
@@ -60,23 +53,5 @@ namespace NewTodo.Test.Controllers
 
             Assert.IsType<NoContentResult>(action);
         }
-
-        [Theory]
-        [MemberData(nameof(InvalidNewTodoInput))]
-        public async Task ShouldReturnInvalid_IfProvideInvalidNewTodoInput(NewTodoInput invalidInput)
-        {
-            var result = await _newTodoInputValidator.TestValidateAsync(invalidInput);
-            Assert.False(result.IsValid);
-        }
-
-        public static IEnumerable<object[]> InvalidNewTodoInput =>
-            new List<object[]>
-            {
-                new object[] {new NewTodoInput() {UserId = Guid.Empty, Title = ValidTitle}},
-                new object[] {new NewTodoInput() {UserId = Guid.Empty, Title = string.Empty}},
-                new object[] {new NewTodoInput() {UserId = Guid.Empty, Title = null}},
-                new object[] {new NewTodoInput() {UserId = Guid.NewGuid(), Title = string.Empty}},
-                new object[] {new NewTodoInput() {UserId = Guid.NewGuid(), Title = null}}
-            };
     }
 }
