da8f79e030f43c7e161cc6aec74e80d6fe27ca35
Weiling Xie <xietanzhen1@yeah.net>
2021-08-06 09:49:43 +1200

    Made EntityBase abstract.

--- src/NewTodo/Domain/Models/EntityBase.cs
+++ src/NewTodo/Domain/Models/EntityBase.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace NewTodo.Domain.Models
 {
-    public class EntityBase
+    public abstract class EntityBase
     {
         public DateTime CreatedAt { get; set; }
         public DateTime LastUpdatedAt { get; set; }
